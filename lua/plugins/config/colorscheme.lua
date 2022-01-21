c18b3edbc144aa7cf5a8363563f55481bb43c44c
Fernando Guerrero <devrrior@gmail.com>
2022-01-21 17:55:33 -0600

    update(lualin): change colorscheme

--- lua/plugins/config/colorscheme.lua
+++ lua/plugins/config/colorscheme.lua
@@ -1,2 +1,5 @@
-vim.g.gruvbox_contrast_dark = 'hard'
-vim.cmd([[colorscheme gruvbox]])
+-- vim.g.gruvbox_contrast_dark = 'hard'
+-- vim.cmd([[colorscheme gruvbox]])
+
+-- Lua
+vim.cmd[[colorscheme catppuccin]]
