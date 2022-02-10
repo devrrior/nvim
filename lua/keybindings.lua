24f3019a6acb291062b07ef04835879c8246eb79
Fernando Guerrero <devrrior@gmail.com>
2022-02-10 08:53:40 -0600

    improve keybindings for telescope

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -84,9 +84,9 @@ keymap('v', '<Leader>/', ':lua require("Comment.api").toggle_linewise_op(vim.fn.
 keymap('n', '<Leader>c',':Bdelete!<CR>',opts)
 
 -- Telescope
-keymap('n', '<C-P>', ':lua require("telescope.builtin").find_files(require("telescope.themes").get_ivy({}))<CR>', opts)
-keymap('n', '<C-F>', ':lua require("telescope.builtin").live_grep(require("telescope.themes").get_ivy({}))<CR>', opts)
-keymap('n', '<C-B>', ':lua require("telescope.builtin").buffers(require("telescope.themes").get_ivy({}))<CR>', opts)
+keymap('n', '<C-P>', ':Telescope find_files<CR>', opts)
+keymap('n', '<C-F>', ':Telescope live_grep<CR>', opts)
+keymap('n', '<C-B>', ':Telescope buffers<CR>', opts)
 keymap('n', '<C-T>', ':lua require("telescope").extensions.git_worktree.git_worktrees(require("telescope.themes").get_ivy({}))<CR>', opts)
 
 -- QuickFix
