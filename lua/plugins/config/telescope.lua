29e7e50563ff242d58fd292d6a0f4548ffa47fd4
Fernando Guerrero <devrrior@gmail.com>
2022-02-10 08:53:14 -0600

    change double quote for quote

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -7,7 +7,7 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
+    file_ignore_patterns = { "__pycache__", "env"},
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
@@ -90,10 +90,16 @@ telescope.setup {
         override_file_sorter = true,     -- override the file sorter
         case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
                                          -- the default case_mode is "smart_case"
+      },
+      media_files = {
+      -- filetypes whitelist
+      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+      -- filetypes = {"png", "webp", "jpg", "jpeg"},
+      find_cmd = "rg" -- find command (defaults to `fd`)
       }
     }
 
 }
 
-telescope.load_extension("git_worktree")
+telescope.load_extension('git_worktree')
 telescope.load_extension('fzf')
