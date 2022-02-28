dabbd1b526c9959dbee710e6c225ce10d65c30bb
Fernando Guerrero <devrrior@gmail.com>
2022-02-28 00:10:44 -0600

    add custom snippets for react in js and add snippets for htmldjango

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -8,7 +8,8 @@ if not snip_status_ok then
   return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+vim.o.runtimepath = vim.o.runtimepath .. ',/Users/fernando/.config/nvim/my-snippets,'
+require("luasnip.loaders.from_vscode").lazy_load()
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
@@ -64,7 +65,7 @@ cmp.setup {
     },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.mapping.confirm { select = false },
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
