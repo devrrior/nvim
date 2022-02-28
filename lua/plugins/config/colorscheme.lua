b3bb5a39d21cacb855c9a0c1bafb787cc1c47398
Fernando Guerrero <devrrior@gmail.com>
2022-02-27 23:34:14 -0600

    change gruvbox theme to onedark theme

--- lua/plugins/config/colorscheme.lua
+++ lua/plugins/config/colorscheme.lua
@@ -2,4 +2,9 @@
 -- vim.cmd([[colorscheme gruvbox]])
 
 -- Lua
-vim.cmd[[colorscheme catppuccin]]
+vim.cmd([[colorscheme onedarker]])
+vim.g.transparent_background = true        -- transparent background(Default: false)
+vim.g.italic_comments = true               -- italic comments(Default: true)
+vim.g.italic_keywords = true               -- italic keywords(Default: true)
+vim.g.italic_functions = true              -- italic functions(Default: false)
+vim.g.italic_variables = true              -- italic variables(Default: false)
