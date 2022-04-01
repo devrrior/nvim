6828b3c197a9d089684c9a1598113a1d24ec213e
Fernando Guerrero <devrrior@gmail.com>
2022-04-01 14:05:47 -0600

    fix(colorscheme): add new config

--- lua/plugins/config/colorscheme.lua
+++ lua/plugins/config/colorscheme.lua
@@ -3,12 +3,12 @@
 
 -- Lua
 -- chris
--- vim.g.transparent_background = true        -- transparent background(Default: false)
--- vim.g.italic_comments = true               -- italic comments(Default: true)
--- vim.g.italic_keywords = true               -- italic keywords(Default: true)
--- vim.g.italic_functions = true              -- italic functions(Default: false)
--- vim.g.italic_variables = true              -- italic variables(Default: false)
--- vim.cmd([[colorscheme onedarker]])
+vim.g.transparent_background = true        -- transparent background(Default: false)
+vim.g.italic_comments = true               -- italic comments(Default: true)
+vim.g.italic_keywords = true               -- italic keywords(Default: true)
+vim.g.italic_functions = true              -- italic functions(Default: false)
+vim.g.italic_variables = true              -- italic variables(Default: false)
+vim.cmd([[colorscheme darkplus]])
 
 
 -- Example config in Lua
@@ -31,38 +31,38 @@
 
 
 -- Lua
-require('onedark').setup  {
-    -- Main options --
-    style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = false,  -- Show/hide background
-    term_colors = true, -- Change terminal color as per the selected theme style
-    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    -- toggle theme style ---
-    toggle_style_key = '<leader>ts', -- Default keybinding to toggle
-    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
-
-    -- Change code style ---
-    -- Options are italic, bold, underline, none
-    -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
-    code_style = {
-        comments = 'italic',
-        keywords = 'italic',
-        functions = 'none',
-        strings = 'italic',
-        variables = 'none'
-    },
-
-    -- Custom Highlights --
-    colors = {}, -- Override default colors
-    highlights = {}, -- Override highlight groups
-
-    -- Plugins Config --
-    diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true,   -- use undercurl instead of underline for diagnostics
-        background = true,    -- use background color for virtual text
-    },
-}
-
-require('onedark').load()
-
+-- require('onedark').setup  {
+--     -- Main options --
+--     style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+--     transparent = false,  -- Show/hide background
+--     term_colors = true, -- Change terminal color as per the selected theme style
+--     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+--     -- toggle theme style ---
+--     toggle_style_key = '<leader>ts', -- Default keybinding to toggle
+--     toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+--
+--     -- Change code style ---
+--     -- Options are italic, bold, underline, none
+--     -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
+--     code_style = {
+--         comments = 'italic',
+--         keywords = 'italic',
+--         functions = 'none',
+--         strings = 'italic',
+--         variables = 'none'
+--     },
+--
+--     -- Custom Highlights --
+--     colors = {}, -- Override default colors
+--     highlights = {}, -- Override highlight groups
+--
+--     -- Plugins Config --
+--     diagnostics = {
+--         darker = true, -- darker colors for diagnostic
+--         undercurl = true,   -- use undercurl instead of underline for diagnostics
+--         background = true,    -- use background color for virtual text
+--     },
+-- }
+--
+-- require('onedark').load()
+--
