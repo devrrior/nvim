1efb297e283459e569a6ab85f37f5c14bedb95a4
Fernando Guerrero <devrrior@gmail.com>
2022-05-01 13:23:39 -0500

    feat(lualine): change theme

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -127,7 +127,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-    theme = "tokyonight",
+    theme = "gruvbox",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
