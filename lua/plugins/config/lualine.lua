afc5ad5d26ddac913ee23e8eef0994e2798024d4
Fernando Guerrero <devrrior@gmail.com>
2022-06-17 16:44:00 -0500

    fix(lualine): update lualine theme

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -127,7 +127,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-    theme = "gruvbox",
+    theme = custom_theme,
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
